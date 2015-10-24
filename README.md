7d09228601cf6e158ff252d29b7b61911eab9efc
Thomas <thomas.white13@gmail.com>
2015-10-24 02:10:35 -0400

    Updated function for Complex Wires, adding input checking and support for users without colorama installed

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Tools for the game Keep Talking and Nobody Explodes
 ##Requirements
  - Python 2.7.10
  - pip
- - colorama
+ - colorama (optional)
 
 ##Execution
 Execute the applicable script from the command line. These tools use colorama to enhance the output to the terminal. Executing a script will prompt the user to enter some details about the given bomb, and return formatted text to the terminal regarding how to proceed.
-
-Currently, a script has been implemented for Complex Wires. A script for memory is in progress. A future version will remove the requirement for colorama in the event the user does not have it installed.
+Users who do not have colorama installed will be advised to install it, but the script can still run without colorama.
+Currently, a script has been implemented for Complex Wires. A script for memory is in progress.
--- complexWires.py
+++ complexWires.py
@@ -1,45 +1,63 @@
-from colorama import init, Fore, Back
+#complexWires.py
+#Display a truth table for the complex wires in the game Keep Talking and Nobody Explodes
 
-init()  #initialize coloramas
-
-parallel    = raw_input('Parallel Port? (y/n) ')
-evenSerial  = raw_input('Even Serial No.? (y/n) ')
-twoPlusBatt = raw_input('2 or more batteries? (y/n) ')
-
-colorNo  = Back.RED   + ' No  ' + Back.RESET
-colorYes = Back.GREEN + ' Yes ' + Back.RESET
-
+#Check whether colorama is installed, set variabeles appropriately
+try:
+  from colorama import init, Back
+  init()
+  cutNo  = Back.RED   + ' No  ' + Back.RESET
+  cutYes = Back.GREEN + ' Cut ' + Back.RESET
+except ImportError:
+  cutNo  = ' No  '
+  cutYes = ' Cut '
+  print 'You should seriously consider installing colorama.'
 
 #[led/star][red/blue]
 truthTable  = [[1,0,0,0],[1,1,0,0],[0,0,0,0],[0,0,0,0]]
 
-if(parallel.lower() == 'y'):
-  truthTable[1][3] = 1
-  truthTable[2][2] = 1
-  truthTable[3][2] = 1
-else:
-  if(parallel.lower() != 'n'):
-    print 'RESULTS MAY BE INACCURATE'
+#Prompt the user for information about the bomb (Parallel ports, Serial number, Batteries)
+invalidInput = True
+while(invalidInput):
+  parallel = raw_input('Parallel Port? (y/n) ')
+  if(parallel.lower() == 'y'):
+    truthTable[1][3] = 1
+    truthTable[2][2] = 1
+    truthTable[3][2] = 1
+    invalidInput = False
+  elif(parallel.lower() == 'n'):
+    invalidInput = False
+  else:
+    print 'Invalid input, retry.'
 
-if(evenSerial.lower() == 'y'):
-  truthTable[0][1] = 1
-  truthTable[0][2] = 1
-  truthTable[0][3] = 1
-  truthTable[2][3] = 1
-else:
-  if(evenSerial.lower() != 'n'):
-    print 'RESULTS MAY BE INACCURATE'
+invalidInput = True
+while(invalidInput):
+  evenSerial  = raw_input('Even Serial No.? (y/n) ')
+  if(evenSerial.lower() == 'y'):
+    truthTable[0][1] = 1
+    truthTable[0][2] = 1
+    truthTable[0][3] = 1
+    truthTable[2][3] = 1
+    invalidInput = False
+  elif(evenSerial.lower() == 'n'):
+    invalidInput = False
+  else:
+    print 'Invalid input, retry.'
 
-if(twoPlusBatt.lower() == 'y'):
-  truthTable[2][1] = 1
-  truthTable[3][0] = 1
-  truthTable[3][1] = 1
-else:
-  if(twoPlusBatt.lower() != 'n'):
-    print 'RESULTS MAY BE INACCURATE'    
+invalidInput = True
+while(invalidInput):
+  twoPlusBatt = raw_input('2 or more batteries? (y/n) ')
+  if(twoPlusBatt.lower() == 'y'):
+    truthTable[2][1] = 1
+    truthTable[3][0] = 1
+    truthTable[3][1] = 1
+    invalidInput = False
+  elif(twoPlusBatt.lower() == 'n'):
+    invalidInput = False
+  else:
+    print 'Invalid input, retry.'
 
 #Print a header
-print '             White Red  Blue R/B '
+print '\r\n             White Red  Blue R/B '
 
 for x in xrange(0,4):
   if(x == 0):
@@ -52,9 +70,9 @@ for x in xrange(0,4):
     printThis = '   LED,    * '
   for y in xrange(0,4): 
     if(truthTable[x][y] == 1):
-      printThis += colorYes
+      printThis += cutYes
     else:
-      printThis += colorNo
+      printThis += cutNo
     
   print printThis
   
